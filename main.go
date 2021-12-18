46dbe23eb5680df1a7d215a80221d30461f372ca
kevinxcode <kevinalnizar@gmail.com>
2021-12-18 15:40:13 +0700

    new commit

--- main.go
+++ main.go
@@ -6,13 +6,13 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		n, err := fmt.Fprintf(w, "hello, world!")
-		if err != nil{
+		if err != nil {
 			fmt.Println(err)
 		}
 		fmt.Println(fmt.Sprintf("number of bytes written : %d", n))
 	})
 
-	_ = http.ListenAndServe(":8080", nil) // running on web browser
+	_ = http.ListenAndServe(":8080", nil) // running on web browser aa
 }

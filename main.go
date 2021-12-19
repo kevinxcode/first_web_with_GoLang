e55a57ec1400e1c280828a15a00ddc293cd62ddc
kevinxcode <kevinalnizar@gmail.com>
2021-12-19 13:21:33 +0700

    Add error handler

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
 
 var portNumber = ":8080"
+
 // home is home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "this is the home page")
@@ -21,6 +23,25 @@ func About(w http.ResponseWriter, r *http.Request) {
 	_, _ = fmt.Fprintf(w, fmt.Sprintf("this is about page and 2 + 2 is %d", sum))
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(100.0, 0.0)
+	if err != nil {
+		fmt.Fprintf(w, "cannot divide by 0 ")
+		return
+	}
+	fmt.Fprintf(w, fmt.Sprintf("%f divide by %f is %f", 100.0, 0.0, f))
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("cannot divide by zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+
+}
+
 // add values twi integer and return sum
 func AddValues(x, y int) int {
 	return x + y
@@ -31,6 +52,7 @@ func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
 	http.HandleFunc("/profile", Profile)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
